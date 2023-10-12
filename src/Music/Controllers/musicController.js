a43db69004d2d120365a9b60c3ea7b3e0e5231c0
Adriano Nobre <adrianonobrevg02@gmail.com>
2023-10-12 12:23:53 -0400

    Update musicController.js

--- src/Music/Controllers/musicController.js
+++ src/Music/Controllers/musicController.js
@@ -1,88 +1,136 @@
-const axios = require('axios');
-const Music = require('../Models/music');
-const UserData = require('../Models/music');
+const axios = require("axios");
+const Music = require("../Models/music");
+const UserData = require("../Models/music");
 
 const getRecentlyPlayedTracks = async (req, res) => {
   const { access_token } = req.params;
   try {
     // Obter dados do usuário
-    const userResponse = await axios.get('https://api.spotify.com/v1/me', {
+    const userResponse = await axios.get("https://api.spotify.com/v1/me", {
       headers: {
-        'Authorization': `Bearer ${access_token}`,
+        Authorization: `Bearer ${access_token}`,
       },
     });
 
-    // Obter dados das músicas recentemente tocadas
-    const musicResponse = await axios.get('https://api.spotify.com/v1/me/player/recently-played', {
-      headers: {
-        'Authorization': `Bearer ${access_token}`,
-      },
-    });
+    let newUserCreated = false;
+    // Verificar se o usuário existe no banco de dados
+    let user = await UserData.findOne({ spotifyId: userResponse.data.id });
 
-    const currentDate = new Date(); // Data e hora atuais
+    // Se o usuário não existe, crie um novo documento UserData para ele
+    if (!user) {
+      user = new UserData({
+        spotifyId: userResponse.data.id,
+        displayName: userResponse.data.display_name,
+        email: userResponse.data.email,
+        musicData: [],
+      });
+      newUserCreated = true;
+      await user.save(); // Salva o novo usuário no banco de dados
+    }
 
-    // Filtra apenas as músicas tocadas nas últimas 24 horas
-    const recentlyPlayedTracks = musicResponse.data.items.filter(item => {
-      const playedAt = new Date(item.played_at);
-      const timeDifference = currentDate - playedAt;
-      const hoursDifference = timeDifference / (1000 * 60 * 60); // Diferença em horas
+    // Obter dados da última música tocada
+    const musicResponse = await axios.get(
+      "https://api.spotify.com/v1/me/player/recently-played",
+      {
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+        },
+      }
+    );
 
-      return hoursDifference <= 24; // Retorna true se a música foi tocada nas últimas 24 horas
-    });
+    // Obter a última música tocada
+    const lastPlayedTrack = musicResponse.data.items[0];
 
-    // Mapeie as músicas para o formato esperado pelo esquema Music
-    const musicData = recentlyPlayedTracks.map(item => {
-      return {
-        name: item.track.name,
-        artists: item.track.artists.map(artist => artist.name).join(', '),
-        album: item.track.album.name,
-        imageUrl: item.track.album.images.length > 0 ? item.track.album.images[0].url : null,
-        playedAt: item.played_at,
-      };
-    });
-
-    // Salva as músicas e os dados do usuário no banco de dados
-    const user = new UserData({
+    // Verificar se a música já está no banco de dados pelo played_at
+    const existingMusic = await UserData.findOne({
       spotifyId: userResponse.data.id,
-      displayName: userResponse.data.display_name,
-      email: userResponse.data.email,
-      musicData: musicData,
+      "musicData.playedAt": lastPlayedTrack.played_at,
     });
 
-    await user.save();
+    // Se a música já existe no banco de dados, não faça nada
+    if (existingMusic) {
+      res.json({ message: "Última música já salva no banco de dados." });
+      return;
+    }
 
-    res.json(recentlyPlayedTracks);
+    // Adicionar a última música ao histórico de músicas do usuário
+    const updateResult = await UserData.updateOne(
+      { spotifyId: userResponse.data.id },
+      {
+        $push: {
+          musicData: {
+            name: lastPlayedTrack.track.name,
+            artists: lastPlayedTrack.track.artists
+              .map((artist) => artist.name)
+              .join(", "),
+            album: lastPlayedTrack.track.album.name,
+            imageUrl:
+              lastPlayedTrack.track.album.images.length > 0
+                ? lastPlayedTrack.track.album.images[0].url
+                : null,
+            playedAt: lastPlayedTrack.played_at,
+          },
+        },
+      },
+      { upsert: true } // Esta opção cria o documento se ele não existir
+    );
+
+
+    if (newUserCreated === true && updateResult.modifiedCount > 0){
+      res.json({ message: 'Usuário criado e última música adicionada ao histórico.'});
+      newUserCreated == false;
+    }
+
+    if (updateResult.modifiedCount > 0){
+      res.json({ message: 'Última música adicionada ao histórico.'});
+    } else{
+      res.status(500).json({ message: 'Erro ao adicionar música ao histórico.'});
+    }
+
   } catch (error) {
-    console.error('Erro ao obter músicas escutadas recentemente:', error.response ? error.response.data : error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error(
+      "Erro ao obter música escutada recentemente:",
+      error.response ? error.response.data : error
+    );
+    res
+      .status(500)
+      .json({ error: "Internal Server Error", newUserCreated: false });
   }
 };
 
-
-
 const getCurrentlyPlayingTrack = async (req, res) => {
-
   const { access_token } = req.params;
   try {
-    const response = await axios.get('https://api.spotify.com/v1/me/player/currently-playing', {
-      headers: {
-        'Authorization': `Bearer ${access_token}`,
-      },
-    });
+    const response = await axios.get(
+      "https://api.spotify.com/v1/me/player/currently-playing",
+      {
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+        },
+      }
+    );
 
     if (response.data && response.data.item) {
       const currentlyPlayingTrack = {
         name: response.data.item.name,
-        artists: response.data.item.artists.map(artist => artist.name).join(', '),
+        artists: response.data.item.artists
+          .map((artist) => artist.name)
+          .join(", "),
         album: response.data.item.album.name,
-        imageUrl: response.data.item.album.images.length > 0 ? response.data.item.album.images[0].url : null,
+        imageUrl:
+          response.data.item.album.images.length > 0
+            ? response.data.item.album.images[0].url
+            : null,
       };
       res.json(currentlyPlayingTrack);
     } else {
       return null; // Retorna null se não estiver escutando nada no momento
     }
   } catch (error) {
-    console.error('Erro ao obter música atualmente em reprodução:', error.response ? error.response.data : error);
+    console.error(
+      "Erro ao obter música atualmente em reprodução:",
+      error.response ? error.response.data : error
+    );
     throw error; // Propaga o erro para que seja tratado onde a função foi chamada
   }
 };
@@ -91,5 +139,3 @@ module.exports = {
   getRecentlyPlayedTracks,
   getCurrentlyPlayingTrack,
 };
-
-
